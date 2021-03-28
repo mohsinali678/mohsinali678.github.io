a2da2807d94f847c40cdfc43f8f68058ce3c2257
Hafiz Ali <mohsantahir@yahoo.com>
2021-03-28 15:52:45 -0400

    Animation

--- AJAX Practice/javascript.js
+++ AJAX Practice/javascript.js
@@ -81,20 +81,20 @@
 
 // Lesson 5: Animating Content
 
-// $("#circle")
-// .click(function (){
-//     $(this)
-//     .animate({
-//         width:"400px",
-//         height:"400px",
-//         marginLeft:"100px",
-//         marginTop:"100px"
-//     }, 2000,    
-//     function(){
-//         $(this).css("background-color", "red");
-//     })
+$("#circle")
+.click(function (){
+    $(this)
+    .animate({
+        width:"400px",
+        height:"400px",
+        marginLeft:"100px",
+        marginTop:"100px"
+    }, 2000,    
+    function(){
+        $(this).css("background-color", "red");
+    })
 
-// })
+})
 
 
 // Lesson 5: AJAX
